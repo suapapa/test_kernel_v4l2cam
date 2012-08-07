9ffd317ffb79996464b7cdee47492a57ad78d51f
Homin Lee <suapapa@insignal.co.kr>
2012-08-07 17:22:09 +0900

    Added Android.mk and fixed indent

---
+++ Android.mk
@@ -0,0 +1,14 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES += linaroca.c
+
+LOCAL_C_INCLUDES += hardware/samsung/exynos4/hal/include
+LOCAL_SHARED_LIBRARIES += libutils libui liblog libbinder libdl libcutils
+
+LOCAL_SHARED_LIBRARIES += libjpeg
+LOCAL_C_INCLUDES += external/jpeg
+
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := linaroca
+include $(BUILD_EXECUTABLE)
